3e70d43c91042ec75f9a298ab52051202da589b1
LeoMartine <83520074+LeoMartine@users.noreply.github.com>
2021-05-02 21:23:05 -0400

    +script para barra

--- js/script.js
+++ js/script.js
@@ -49,3 +49,13 @@ function validarApellido(){
     }    
     console.log ('apellido:'+ apellido);
 }
+
+var searchbar = document.querySelector("#search");
+function slide(){
+    
+    if(searchbar.classList.contains("hide")){
+        searchbar.classList.remove("hide");
+    }else{
+         searchbar.classList.add("hide");
+    }
+}
